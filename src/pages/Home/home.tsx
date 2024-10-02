cf06780fa341ca6d2dcedc2d7f87fb9babca95e7
Carol Tagliaferro <caroltagliaferro1@gmail.com>
2024-10-02 17:57:59 -0300

    Add newsletter na home

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import FirstSection from "./components/firstSection";
 import SecondSection from "./components/secondSection";
+import ThirdSection from "./components/thirdSection";
 
 const Home = () => {
   return (
     <div>
       <FirstSection />
       <SecondSection />
+      <ThirdSection />
     </div>
   );
 };
