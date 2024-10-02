5d8cba7a6259e4632095fec3b160d5052b377e97
Carol Tagliaferro <caroltagliaferro1@gmail.com>
2024-10-02 16:59:18 -0300

    add página about us

---
+++ src/pages/Sobre nós/sobreNos.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FirstSection from "./components/firstSection";
+
+const SobreNos = () => {
+  return (
+    <div>
+      <FirstSection />
+    </div>
+  );
+};
+
+export default SobreNos;
