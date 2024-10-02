b2ec171df513403df8a489a071aec1c93993fa6a
Carol Tagliaferro <caroltagliaferro1@gmail.com>
2024-10-02 16:59:40 -0300

    fix card de receitas

--- src/components/recipeCard.tsx
+++ src/components/recipeCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 interface Recipe {
   id: number;
@@ -13,14 +14,18 @@ interface RecipeCardProps {
 
 const RecipeCard: React.FC<RecipeCardProps> = ({ recipes }) => {
   return (
-    <div className="bg-bgWhite w-80 rounded-lg shadow-md p-2">
-      <img
-        src={recipes.image}
-        alt={recipes.name}
-        className="w-96 h-48 object-cover rounded-t-lg"
-      />
-      <h2 className="mt-2 text-lg text-title font-semibold">{recipes.name}</h2>
-    </div>
+    <Link to={`/recipe/${recipes.id}`}>
+      <div className="bg-bgWhite w-80 rounded-lg shadow-md p-2 hover:brightness-90">
+        <img
+          src={recipes.image}
+          alt={recipes.name}
+          className="w-96 h-48 object-cover rounded-t-lg"
+        />
+        <h2 className="mt-2 text-lg text-title font-semibold">
+          {recipes.name}
+        </h2>
+      </div>
+    </Link>
   );
 };
 
