d9cc2c40134a60359385f128e54715b6d39a1ab0
Carol Tagliaferro <caroltagliaferro1@gmail.com>
2024-10-04 18:08:28 -0300

    rotas ajustadas

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,6 +4,8 @@ import Receitas from "../pages/Receitas/receitas";
 import RecipeDetails from "../components/recipeDetails";
 import SobreNos from "../pages/Sobre nós/sobreNos";
 import Dicas from "../pages/Dicas/dicas";
+import KidsRecipes from "../components/kidsRecipes";
+import ImunidadeAlimentos from "../components/imunidadeAlimentos";
 
 function Router() {
   return (
@@ -13,6 +15,8 @@ function Router() {
       <Route path="/dicas" element={<Dicas />} />
       <Route path="/us" element={<SobreNos />} />
       <Route path="/recipe/:id" element={<RecipeDetails />} />
+      <Route path="/kids" element={<KidsRecipes />} />
+      <Route path="/imuni" element={<ImunidadeAlimentos />} />
     </Routes>
   );
 }
