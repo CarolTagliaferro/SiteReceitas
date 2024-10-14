c6aad339f2119562526c7b0d2f8ba82ef743e020
Carol Tagliaferro <caroltagliaferro1@gmail.com>
2024-10-14 17:58:46 -0300

    add carrossel na home

--- src/pages/Home/components/secondSection.tsx
+++ src/pages/Home/components/secondSection.tsx
@@ -2,37 +2,39 @@ import React from "react";
 import RecipeCard from "../../../components/recipeCard";
 import recipes from "../../../services/recipes.json";
 import { NavLink } from "react-router-dom";
-
-interface Recipe {
-  id: number;
-  name: string;
-  tags: string[];
-  image: string;
-}
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 const SecondSection: React.FC = () => {
   return (
-    <div className="my-5">
-      <div>
-        <h1 className="text-center font-semibold text-3xl text-title">
-          Nossas receitas:
-        </h1>
-      </div>
-      <div>
-        <div className="flex justify-center gap-14 my-5">
-          {recipes.recipes.slice(0, 4).map((recipes: Recipe) => (
-            <RecipeCard key={recipes.id} recipes={recipes} />
-          ))}
-        </div>
-        <div className="text-center">
-          <NavLink to="/receitas">
-            <button className="bg-bgDark py-3 px-4 rounded-xl text-bgWhite font-medium text-lg hover:bg-bgLight transition-all">
-              Veja mais
-            </button>
-          </NavLink>
-        </div>
+    <div className="my-10 mx-20">
+      <h1 className="text-center font-semibold text-3xl text-title">
+        Nossas receitas:
+      </h1>
+      <Splide
+        options={{
+          type: "loop",
+          perPage: 5,
+          drag: "free",
+        }}
+        className="my-10"
+      >
+        {recipes.recipes.slice(0, 6).map((recipe) => (
+          <SplideSlide key={recipe.id}>
+            <RecipeCard recipes={recipe} />
+          </SplideSlide>
+        ))}
+      </Splide>
+
+      <div className="text-center">
+        <NavLink to="/receitas">
+          <button className="bg-bgDark py-3 px-4 rounded-xl text-bgWhite font-medium text-lg hover:bg-bgLight transition-all">
+            Veja mais
+          </button>
+        </NavLink>
       </div>
     </div>
   );
 };
+
 export default SecondSection;
